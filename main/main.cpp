fec9039b0b2120c21f59a1db8ce459502c6909cb
Alex <alex@neoxelox.com>
2022-10-07 01:03:02 +0200

    Add base app main entrypoint

--- main/main.cpp
+++ main/main.cpp
@@ -1,42 +1,45 @@
-#include <stdio.h>
 #include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
+#include "esp_app_desc.h"
+#include "esp_timer.h"
+#include "logger.hpp"
+
+namespace main
+{
+    static const char *TAG = "main";
+
+    class App
+    {
+    private:
+        const esp_app_desc_t *description;
+        logger::Logger logger;
+
+    public:
+        static App New()
+        {
+            App app;
+
+            int64_t elapsed = esp_timer_get_time();
+
+            app.description = esp_app_get_description();
+
+            // Inject dependencies
+
+            app.logger = logger::Logger::New((esp_log_level_t)CONFIG_LOG_DEFAULT_LEVEL);
+
+            elapsed = esp_timer_get_time() - elapsed;
+            app.logger.Info(TAG, "Startup took %f ms", (float)(elapsed / 1000.0l));
+            app.logger.Info(TAG, "==================== %s v%s ====================", app.description->project_name, app.description->version);
+
+            return app;
+        }
+    };
+}
+
+// App dependencies container
+static main::App app;
 
 extern "C" void app_main(void)
 {
-    printf("Hello world!\n");
-
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), WiFi%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-
-    printf("silicon revision %d, ", chip_info.revision);
-    if (esp_flash_get_size(NULL, &flash_size) != ESP_OK)
-    {
-        printf("Get flash size failed");
-        return;
-    }
-
-    printf("%uMB %s flash\n", flash_size / (1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %d bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--)
-    {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    // Dependencies must be initialized after App main entrypoint
+    app = main::App::New();
 }
