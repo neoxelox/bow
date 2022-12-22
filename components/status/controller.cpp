5fce03149242aedebf20d3a25789f0e75bb27a3f
Alex <alex@neoxelox.com>
2022-12-22 21:44:51 +0100

    Finish status controller

--- components/status/controller.cpp
+++ components/status/controller.cpp
@@ -1,6 +1,9 @@
 #include "esp_err.h"
 #include "driver/gpio.h"
 #include "driver/ledc.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 #include "logger.hpp"
 #include "gpio.hpp"
 #include "status.hpp"
@@ -9,14 +12,79 @@ namespace status
 {
     Controller *Controller::New(logger::Logger *logger)
     {
-        Controller *controller = new Controller();
+        if (Instance != NULL)
+            return Instance;
 
-        controller->logger = logger;
+        Instance = new Controller();
 
-        controller->redPin = gpio::Analog::New(GPIO_NUM_14, GPIO_MODE_OUTPUT, LEDC_TIMER_0, LEDC_TIMER_8_BIT, 1000);
-        controller->greenPin = gpio::Analog::New(GPIO_NUM_13, GPIO_MODE_OUTPUT, LEDC_TIMER_0, LEDC_TIMER_8_BIT, 1000);
-        controller->bluePin = gpio::Analog::New(GPIO_NUM_12, GPIO_MODE_OUTPUT, LEDC_TIMER_0, LEDC_TIMER_8_BIT, 1000);
+        // Inject dependencies
+        Instance->logger = logger;
 
-        return controller;
+        // Initialize RGB LED
+        Instance->redPin = gpio::Analog::New(GPIO_NUM_14, GPIO_MODE_OUTPUT, LEDC_TIMER_0, LEDC_TIMER_8_BIT, 1000);
+        Instance->greenPin = gpio::Analog::New(GPIO_NUM_13, GPIO_MODE_OUTPUT, LEDC_TIMER_0, LEDC_TIMER_8_BIT, 1000);
+        Instance->bluePin = gpio::Analog::New(GPIO_NUM_12, GPIO_MODE_OUTPUT, LEDC_TIMER_0, LEDC_TIMER_8_BIT, 1000);
+        Instance->redPin->SetLevel(0);
+        Instance->greenPin->SetLevel(0);
+        Instance->bluePin->SetLevel(0);
+
+        // Initialize RGB LED queue
+        Instance->queue = xQueueCreate(QUEUE_SIZE, sizeof(Status));
+        if (!Instance->queue)
+            ESP_ERROR_CHECK(ESP_ERR_NO_MEM);
+
+        // Create RGB LED task
+        xTaskCreatePinnedToCore(Instance->taskFunc, "Status", 4 * 1024, NULL, 5, &Instance->taskHandle, tskNO_AFFINITY);
+
+        return Instance;
+    }
+
+    void Controller::setColor(Color color, uint8_t brightness)
+    {
+        this->redPin->SetLevel((color.Red * brightness) / 100);
+        this->greenPin->SetLevel((color.Green * brightness) / 100);
+        this->bluePin->SetLevel((color.Blue * brightness) / 100);
+    }
+
+    esp_err_t Controller::SetStatus(Status status)
+    {
+        if (!status.Duration && !status.Loop)
+            return ESP_ERR_INVALID_ARG;
+
+        status.Brightness = status.Brightness <= 100 ? status.Brightness : 100;
+
+        if (xQueueSend(this->queue, &status, 0) == errQUEUE_FULL)
+            return ESP_ERR_NO_MEM;
+
+        return ESP_OK;
+    }
+
+    void Controller::taskFunc(void *args)
+    {
+        Status backStatus = Statuses::Off;
+        Status frontStatus = Statuses::Off;
+        bool toggle = false;
+
+        while (1)
+        {
+            if (xQueueReceive(Instance->queue, &frontStatus,
+                              (frontStatus.Duration >= MIN_TASK_WAIT ? frontStatus.Duration : MIN_TASK_WAIT) / portTICK_PERIOD_MS))
+            {
+                toggle = false;
+                if (frontStatus.Loop)
+                    backStatus = frontStatus;
+            }
+
+            if ((toggle = !toggle) || frontStatus.Duration == NULL)
+                Instance->setColor(frontStatus.Color, frontStatus.Brightness);
+            else if (frontStatus.Loop)
+                Instance->setColor(Statuses::Off.Color, Statuses::Off.Brightness);
+            else
+            {
+                Instance->setColor(backStatus.Color, backStatus.Brightness);
+                toggle = true;
+                frontStatus = backStatus;
+            }
+        }
     }
 }
--- components/status/status.hpp
+++ components/status/status.hpp
@@ -1,11 +1,56 @@
 #pragma once
 
+#include "esp_err.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 #include "logger.hpp"
 #include "gpio.hpp"
 
 namespace status
 {
     static const char *TAG = "status";
+
+    static const uint32_t MIN_TASK_WAIT = 125; // Milliseconds
+
+    static const int QUEUE_SIZE = 25;
+
+    class Color
+    {
+    public:
+        uint8_t Red;
+        uint8_t Green;
+        uint8_t Blue;
+    };
+
+    namespace Colors
+    {
+        static const Color Primary = {8, 0, 255};
+        static const Color Success = {0, 255, 0};
+        static const Color Info = {255, 255, 255};
+        static const Color Warning = {255, 255, 0};
+        static const Color Danger = {255, 0, 0};
+        static const Color None = {0, 0, 0};
+    }
+
+    class Status
+    {
+    public:
+        class Color Color;
+        uint8_t Brightness;
+        uint32_t Duration; // Milliseconds
+        bool Loop;
+    };
+
+    namespace Statuses
+    {
+        static const Status Idle = {Colors::Primary, 100, NULL, true};
+        static const Status Transmitted = {Colors::Success, 100, 250, false};
+        static const Status Received = {Colors::Info, 100, 250, false};
+        static const Status Provisioning = {Colors::Warning, 100, 1000, true};
+        static const Status Error = {Colors::Danger, 100, 500, true};
+        static const Status Off = {Colors::None, 0, NULL, true};
+    }
 
     class Controller
     {
@@ -14,8 +59,18 @@ namespace status
         gpio::Analog *redPin;
         gpio::Analog *greenPin;
         gpio::Analog *bluePin;
+        TaskHandle_t taskHandle;
+        QueueHandle_t queue;
+
+    private:
+        void setColor(Color color, uint8_t brightness);
+        static void taskFunc(void *args);
 
     public:
+        inline static Controller *Instance;
         static Controller *New(logger::Logger *logger);
+
+    public:
+        esp_err_t SetStatus(Status status);
     };
 }
