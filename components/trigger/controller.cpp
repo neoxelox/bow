b6e51a236a6d634b7150aaf53a8b885eb3f837b5
Alex <alex@neoxelox.com>
2022-12-30 00:46:47 +0100

    Add trigger CRUD

---
+++ components/trigger/controller.cpp
@@ -0,0 +1,168 @@
+#include <string.h>
+#include "esp_err.h"
+#include "cJSON.h"
+#include "logger.hpp"
+#include "database.hpp"
+#include "trigger.hpp"
+
+namespace trigger
+{
+    Trigger::Trigger()
+    {
+        this->Name = NULL;
+        this->Actuator = NULL;
+        this->Schedule = NULL;
+        this->Emoji = NULL;
+        this->Creator = NULL;
+        this->CreatedAt = 0;
+    }
+
+    Trigger::Trigger(const char *name, const char *actuator, const char *schedule,
+                     const char *emoji, const char *creator, time_t createdAt)
+    {
+        this->Name = strdup(name);
+        this->Actuator = strdup(actuator);
+        this->Schedule = strdup(schedule);
+        this->Emoji = strdup(emoji);
+        this->Creator = strdup(creator);
+        this->CreatedAt = createdAt;
+    }
+
+    Trigger::Trigger(cJSON *src)
+    {
+        this->Name = strdup(cJSON_GetObjectItem(src, "name")->valuestring);
+        this->Actuator = strdup(cJSON_GetObjectItem(src, "actuator")->valuestring);
+        this->Schedule = strdup(cJSON_GetObjectItem(src, "schedule")->valuestring);
+        this->Emoji = strdup(cJSON_GetObjectItem(src, "emoji")->valuestring);
+        this->Creator = strdup(cJSON_GetObjectItem(src, "creator")->valuestring);
+        this->CreatedAt = cJSON_GetObjectItem(src, "created_at")->valueint;
+    }
+
+    Trigger::~Trigger()
+    {
+        free((void *)this->Name);
+        free((void *)this->Actuator);
+        free((void *)this->Schedule);
+        free((void *)this->Emoji);
+        free((void *)this->Creator);
+    }
+
+    Trigger &Trigger::operator=(const Trigger &other)
+    {
+        if (this == &other)
+            return *this;
+
+        free((void *)this->Name);
+        free((void *)this->Actuator);
+        free((void *)this->Schedule);
+        free((void *)this->Emoji);
+        free((void *)this->Creator);
+
+        this->Name = strdup(other.Name);
+        this->Actuator = strdup(other.Actuator);
+        this->Schedule = strdup(other.Schedule);
+        this->Emoji = strdup(other.Emoji);
+        this->Creator = strdup(other.Creator);
+        this->CreatedAt = other.CreatedAt;
+
+        return *this;
+    }
+
+    cJSON *Trigger::JSON()
+    {
+        cJSON *root = cJSON_CreateObject();
+
+        cJSON_AddStringToObject(root, "name", this->Name);
+        cJSON_AddStringToObject(root, "actuator", this->Actuator);
+        cJSON_AddStringToObject(root, "schedule", this->Schedule);
+        cJSON_AddStringToObject(root, "emoji", this->Emoji);
+        cJSON_AddStringToObject(root, "creator", this->Creator);
+        cJSON_AddNumberToObject(root, "created_at", this->CreatedAt);
+
+        return root;
+    }
+
+    Controller *Controller::New(logger::Logger *logger, database::Database *database)
+    {
+        if (Instance != NULL)
+            return Instance;
+
+        Instance = new Controller();
+
+        // Inject dependencies
+        Instance->logger = logger;
+        Instance->db = database->Open(DB_NAMESPACE);
+
+        return Instance;
+    }
+
+    uint32_t Controller::Count()
+    {
+        uint32_t count;
+
+        ESP_ERROR_CHECK(this->db->Count(&count));
+
+        return count;
+    }
+
+    Trigger *Controller::Get(const char *name)
+    {
+        Trigger *trigger = NULL;
+
+        cJSON *triggerJSON = NULL;
+        ESP_ERROR_CHECK(this->db->Get(name, &triggerJSON));
+
+        if (triggerJSON != NULL)
+            trigger = new Trigger(triggerJSON);
+
+        cJSON_Delete(triggerJSON);
+
+        return trigger;
+    }
+
+    Trigger *Controller::List(uint32_t *size)
+    {
+        uint32_t count = this->Count();
+        *size = count;
+        if (count < 1)
+            return NULL;
+
+        Trigger *list = new Trigger[count];
+        Trigger *aux = list;
+
+        ESP_ERROR_CHECK(this->db->Find(
+            [](const char *key, void *context) -> bool
+            {
+                Trigger *list = *(Trigger **)context;
+
+                cJSON *triggerJSON = NULL;
+                ESP_ERROR_CHECK(Instance->db->Get(key, &triggerJSON));
+
+                *list = Trigger(triggerJSON);
+                (*(Trigger **)context)++;
+
+                cJSON_Delete(triggerJSON);
+                return false;
+            },
+            &aux));
+
+        return list;
+    }
+
+    void Controller::Set(Trigger *trigger)
+    {
+        cJSON *triggerJSON = trigger->JSON();
+        ESP_ERROR_CHECK(this->db->Set(trigger->Name, triggerJSON));
+        cJSON_Delete(triggerJSON);
+    }
+
+    void Controller::Delete(const char *name)
+    {
+        ESP_ERROR_CHECK(this->db->Delete(name));
+    }
+
+    void Controller::Drop()
+    {
+        ESP_ERROR_CHECK(this->db->Drop());
+    }
+}
--- components/trigger/trigger.hpp
+++ components/trigger/trigger.hpp
@@ -1,5 +1,52 @@
 #pragma once
 
+#include "cJSON.h"
+#include "logger.hpp"
+#include "database.hpp"
+
 namespace trigger
 {
+    static const char *TAG = "trigger";
+
+    static const char *DB_NAMESPACE = "trigger";
+
+    class Trigger
+    {
+    public:
+        const char *Name;
+        const char *Actuator;
+        const char *Schedule;
+        const char *Emoji;
+        const char *Creator;
+        time_t CreatedAt;
+
+    public:
+        Trigger();
+        Trigger(const char *name, const char *actuator, const char *schedule,
+                const char *emoji, const char *creator, time_t createdAt);
+        Trigger(cJSON *src);
+        ~Trigger();
+        Trigger &operator=(const Trigger &other);
+        cJSON *JSON();
+    };
+
+    class Controller
+    {
+    private:
+        logger::Logger *logger;
+        database::Handle *db;
+
+    public:
+        inline static Controller *Instance;
+        static Controller *New(logger::Logger *logger, database::Database *database);
+
+    public:
+        uint32_t Count();
+        Trigger *Get(const char *name);
+        Trigger *List(uint32_t *size);
+        void Set(Trigger *trigger);
+        void Delete(const char *name);
+        void Drop();
+        void GenerateToken(char *token);
+    };
 }
